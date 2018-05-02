2dbb33d260006648e8dcc0cbf1eb3294df247af4
Liliane <lilianelou@hotmail.com>
2018-05-02 14:31:21 -0300

    Modificações nos nomes dos descibes e unificação dos testes

--- test/server-test.js
+++ test/server-test.js
@@ -1,33 +1,27 @@
 const supertest = require('supertest');
 const server = require('../server');
 
-describe('server', () => {
-  describe('GET /webhook', () =>
-    it('responds with OK', () =>
+describe('Testes para o server.js', () => {
+
+  describe('GET /webhook', () =>{
+//webhook sem os parâmetros = 403
+    it('responds with no Ok', () =>{
       supertest(server)
         .get('/webhook')
         .expect(403)
-    )
-  );
+    });
+//webhook com os parâmetros inválidos = 403
+    it('responds with no Ok', () =>{
+      supertest(server)
+        .get('/webhook?hub.mode=subs&hub.verify_token=0')
+        .expect(403)
+    });
+//webhook com os parâmetros corretos = 200
+    it('responds with Ok', () =>{
+      supertest(server)
+        .get('/webhook?hub.mode=subscribe&hub.verify_token=subscribe')
+        .expect(200)
+    });
+  });
+  
 });
-
-describe('server', () => {
-    describe('GET /webhook', () =>
-      it('responds with OK', () =>
-        supertest(server)
-          .get('/webhook?hub.mode=subs&hub.verify_token=0')
-          .expect(403)
-      )
-    );
-  });
-
-  describe('server', () => {
-    describe('GET /webhook', () =>
-      it('responds with OK', () =>
-        supertest(server)
-          .get('/webhook?hub.mode=subscribe&hub.verify_token=subscribe')
-          .expect(200)
-      )
-    );
-  });
-
